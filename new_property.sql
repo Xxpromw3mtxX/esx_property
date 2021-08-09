b1818bbabecf5c6a26b6a8afeb80d83dd8956c01
Francesco Metta <fra.metta@hotmail.it>
2021-08-09 20:58:37 +0200

    Update new_property.sql

--- new_property.sql
+++ new_property.sql
@@ -1,46 +1,121 @@
-USE `es_extended`;
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 09, 2021 at 08:56 PM
+-- Server version: 8.0.25-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
 
-ALTER TABLE `users` ADD COLUMN `last_property` VARCHAR(255) NULL;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-INSERT INTO `addon_account` (name, label, shared) VALUES (
-    ('property_black_money','Money Sale Property',0),
-    ('property_money','Money Property',0)
-);
 
-INSERT INTO `addon_inventory` (name, label, shared) VALUES (
-    'property','Property',0
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO `datastore` (name, label, shared) VALUES (
-    'property','Property',0
-);
+--
+-- Database: `es_extended`
+--
 
-DROP TABLE IF EXISTS `owned_properties`;
-CREATE TABLE `owned_properties` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `price` double NOT NULL,
-  `rented` int(11) NOT NULL,
-  `owner` varchar(60) NOT NULL,
-  PRIMARY KEY (`id`)
-);
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `properties`;
+--
+-- Table structure for table `properties`
+--
+
 CREATE TABLE `properties` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `label` varchar(255) DEFAULT NULL,
-  `entering` varchar(255) DEFAULT NULL,
-  `inside` varchar(255) DEFAULT NULL,
-  `outside` varchar(255) DEFAULT NULL,
-  `ipls` varchar(255) DEFAULT '[]',
-  `gateway` varchar(255) DEFAULT NULL,
-  `is_single` int(11) DEFAULT NULL,
-  `is_room` int(11) DEFAULT NULL,
-  `is_gateway` int(11) DEFAULT NULL,
-  `room_menu` varchar(255) DEFAULT NULL,
-  `inventory` varchar(255) DEFAULT NULL,
-  `cloakroom` varchar(255) DEFAULT NULL,
-  `price` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-);
+  `id` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `label` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `entering` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `exit` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `inside` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `outside` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ipls` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '[]',
+  `gateway` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `is_single` int DEFAULT NULL,
+  `is_room` int DEFAULT NULL,
+  `is_gateway` int DEFAULT NULL,
+  `room_menu` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `inventory` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cloakroom` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `price` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `properties`
+--
+
+INSERT INTO `properties` (`id`, `name`, `label`, `entering`, `exit`, `inside`, `outside`, `ipls`, `gateway`, `is_single`, `is_room`, `is_gateway`, `room_menu`, `inventory`, `cloakroom`, `price`) VALUES
+(1, 'WhispymoundDrive', '2677 Whispymound Drive', '{\"y\":564.89,\"z\":182.959,\"x\":119.384}', '{\"x\":117.347,\"y\":559.506,\"z\":183.304}', '{\"y\":557.032,\"z\":183.301,\"x\":118.037}', '{\"y\":567.798,\"z\":182.131,\"x\":119.249}', '[]', NULL, 1, 1, 0, '{\"x\":117.5,\"y\":559.7,\"z\":183.3}', '{\"x\":120.1,\"y\":557.3,\"z\":183.3}', '{\"x\":122.1,\"y\":548.4,\"z\":179.5}', 1500000),
+(2, 'NorthConkerAvenue2045', '2045 North Conker Avenue', '{\"x\":372.796,\"y\":428.327,\"z\":144.685}', '{\"x\":373.548,\"y\":422.982,\"z\":144.907}', '{\"y\":420.075,\"z\":145.904,\"x\":372.161}', '{\"x\":372.454,\"y\":432.886,\"z\":143.443}', '[]', NULL, 1, 1, 0, '{\"x\":373.9,\"y\":423.6,\"z\":144.9}', '{\"x\":375.9,\"y\":420.2,\"z\":144.9}', '{\"x\":374.2,\"y\":410.4,\"z\":141.1}', 1500000),
+(3, 'RichardMajesticApt2', 'Richard Majestic, Apt 2', '{\"y\":-379.165,\"z\":37.961,\"x\":-936.363}', '{\"y\":-365.476,\"z\":113.274,\"x\":-913.097}', '{\"y\":-367.637,\"z\":113.274,\"x\":-918.022}', '{\"y\":-382.023,\"z\":37.961,\"x\":-943.626}', '[]', NULL, 1, 1, 0, '{\"x\":-912.8,\"y\":-365.2,\"z\":113.3}', '{\"x\":-927.554,\"y\":-377.744,\"z\":112.674}', '{\"x\":-903.5,\"y\":-363.7,\"z\":112.1}', 1700000),
+(4, 'NorthConkerAvenue2044', '2044 North Conker Avenue', '{\"y\":440.8,\"z\":146.702,\"x\":346.964}', '{\"y\":437.456,\"z\":148.394,\"x\":341.683}', '{\"y\":435.626,\"z\":148.394,\"x\":339.595}', '{\"x\":350.535,\"y\":443.329,\"z\":145.764}', '[]', NULL, 1, 1, 0, '{\"x\":341.5,\"y\":437.4,\"z\":148.4}', '{\"x\":341.4,\"y\":433.2,\"z\":148.4}', '{\"x\":333.9,\"y\":428.2,\"z\":144.6}\r\n', 1500000),
+(5, 'WildOatsDrive', '3655 Wild Oats Drive', '{\"y\":502.696,\"z\":136.421,\"x\":-176.003}', '{\"y\":497.817,\"z\":136.653,\"x\":-174.349}', '{\"y\":495.069,\"z\":136.666,\"x\":-173.331}', '{\"y\":506.412,\"z\":135.0664,\"x\":-177.927}', '[]', NULL, 1, 1, 0, '{\"y\":497.817,\"z\":136.653,\"x\":-174.349}', '{\"x\":-170.0,\"y\":495.8,\"z\":136.653}', '{\"x\":-166.9,\"y\":488.2,\"z\":132.8}', 1500000),
+(6, 'HillcrestAvenue2862', '2862 Hillcrest Avenue', '{\"y\":596.58,\"z\":142.641,\"x\":-686.554}', '{\"y\":591.988,\"z\":144.392,\"x\":-681.728}', '{\"y\":590.608,\"z\":144.392,\"x\":-680.124}', '{\"y\":599.019,\"z\":142.059,\"x\":-689.492}', '[]', NULL, 1, 1, 0, '{\"x\":-682.5,\"y\":592.6,\"z\":144.4}', '{\"x\":-678.1,\"y\":593.0,\"z\":144.4}', '{\"x\":-671.6,\"y\":587.6,\"z\":140.6}', 1500000),
+(7, 'LowEndApartment', 'Appartement de base', '{\"y\":-1078.735,\"z\":28.4031,\"x\":292.528}', '{\"y\":-1007.152,\"z\":-102.002,\"x\":265.845}', '{\"y\":-1002.802,\"z\":-100.008,\"x\":265.307}', '{\"y\":-1078.669,\"z\":28.401,\"x\":296.738}', '[]', NULL, 1, 1, 0, '{\"x\":266.1,\"y\":-1006.6,\"z\":-99.6}', '{\"x\":265.7,\"y\":-999.7,\"z\":-98.0}', '{\"x\":260.0,\"y\":-1004.0,\"z\":-98.0}', 562500),
+(8, 'MadWayneThunder', '2113 Mad Wayne Thunder', '{\"y\":454.955,\"z\":96.462,\"x\":-1294.433}', '{\"x\":-1289.917,\"y\":449.541,\"z\":96.902}', '{\"y\":446.322,\"z\":96.899,\"x\":-1289.642}', '{\"y\":455.453,\"z\":96.517,\"x\":-1298.851}', '[]', NULL, 1, 1, 0, '{\"x\":-1289.7,\"y\":449.4,\"z\":98.9}', '{\"x\":-1287.7,\"y\":446.6,\"z\":98.9}', '{\"x\":-1286.1,\"y\":438.4,\"z\":93.1}', 1500000),
+(9, 'HillcrestAvenue2874', '2874 Hillcrest Avenue', '{\"x\":-853.346,\"y\":696.678,\"z\":147.782}', '{\"y\":690.875,\"z\":151.86,\"x\":-859.961}', '{\"y\":688.361,\"z\":151.857,\"x\":-859.395}', '{\"y\":701.628,\"z\":147.773,\"x\":-855.007}', '[]', NULL, 1, 1, 0, '{\"x\":-859.7,\"y\":690.9,\"z\":151.9}', '{\"x\":-857.0,\"y\":688.3,\"z\":151.9}', '{\"x\":-855.2,\"y\":680.2,\"z\":148.1}', 1500000),
+(10, 'HillcrestAvenue2868', '2868 Hillcrest Avenue', '{\"y\":620.494,\"z\":141.588,\"x\":-752.82}', '{\"y\":618.62,\"z\":143.153,\"x\":-759.317}', '{\"y\":617.629,\"z\":143.153,\"x\":-760.789}', '{\"y\":621.281,\"z\":141.254,\"x\":-750.919}', '[]', NULL, 1, 1, 0, '{\"x\":-758.2,\"y\":619.0,\"z\":143.1}', '{\"x\":-759.4,\"y\":614.6,\"z\":143.1}', '{\"x\":-767.8,\"y\":610.6,\"z\":139.3}', 1500000),
+(11, 'TinselTowersApt12', 'Tinsel Towers, Apt 42', '{\"y\":37.025,\"z\":42.58,\"x\":-618.299}', '{\"y\":58.898,\"z\":97.2,\"x\":-603.301}', '{\"y\":58.941,\"z\":97.2,\"x\":-608.741}', '{\"y\":30.603,\"z\":42.524,\"x\":-620.017}', '[]', NULL, 1, 1, 0, '{\"y\":58.898,\"z\":97.2,\"x\":-603.301}', '{\"x\":-622.173,\"y\":54.585,\"z\":96.2}', '{\"x\":-594.6,\"y\":55.8,\"z\":96.0}', 1700000),
+(12, 'MiltonDrive', 'Milton Drive', '{\"x\":-775.17,\"y\":312.01,\"z\":84.658}', NULL, NULL, '{\"x\":-775.346,\"y\":306.776,\"z\":84.7}', '[]', NULL, 0, 0, 1, NULL, NULL, NULL, 0),
+(13, 'Modern1Apartment', 'Appartement Moderne 1', NULL, '{\"x\":-784.194,\"y\":323.636,\"z\":210.997}', '{\"x\":-779.751,\"y\":323.385,\"z\":210.997}', NULL, '[\"apa_v_mp_h_01_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-784.3,\"y\":323.6,\"z\":211.0}', '{\"x\":-766.7,\"y\":327.8,\"z\":210.4}', '{\"x\":-793.3,\"y\":327.0,\"z\":209.8}', 1300000),
+(14, 'Modern2Apartment', 'Appartement Moderne 2', NULL, '{\"x\":-786.8663,\"y\":315.764,\"z\":186.913}', '{\"x\":-781.808,\"y\":315.866,\"z\":186.913}', NULL, '[\"apa_v_mp_h_01_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-787.4,\"y\":315.7,\"z\":186.9}', '{\"x\":-795.735,\"y\":326.757,\"z\":186.313}', '{\"x\":-797.8,\"y\":329.1,\"z\":189.7}', 1300000),
+(15, 'Modern3Apartment', 'Appartement Moderne 3', NULL, '{\"x\":-774.012,\"y\":342.042,\"z\":195.686}', '{\"x\":-779.057,\"y\":342.063,\"z\":195.686}', NULL, '[\"apa_v_mp_h_01_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-774.4,\"y\":341.9,\"z\":195.7}', '{\"x\":-765.386,\"y\":330.782,\"z\":195.08}', '{\"x\":-763.2,\"y\":328.6,\"z\":198.5}', 1300000),
+(16, 'Mody1Apartment', 'Appartement Mode 1', NULL, '{\"x\":-784.194,\"y\":323.636,\"z\":210.997}', '{\"x\":-779.751,\"y\":323.385,\"z\":210.997}', NULL, '[\"apa_v_mp_h_02_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-784.1,\"y\":323.7,\"z\":211.0}', '{\"x\":-766.615,\"y\":327.878,\"z\":210.396}', '{\"x\":-793.4,\"y\":327.2,\"z\":209.8}', 1300000),
+(17, 'Mody2Apartment', 'Appartement Mode 2', NULL, '{\"x\":-786.8663,\"y\":315.764,\"z\":186.913}', '{\"x\":-781.808,\"y\":315.866,\"z\":186.913}', NULL, '[\"apa_v_mp_h_02_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-786.5,\"y\":315.8,\"z\":186.9}', '{\"x\":-795.297,\"y\":327.092,\"z\":186.313}', '{\"x\":-797.8,\"y\":328.3,\"z\":189.7}', 1300000),
+(18, 'Mody3Apartment', 'Appartement Mode 3', NULL, '{\"x\":-774.012,\"y\":342.042,\"z\":195.686}', '{\"x\":-779.057,\"y\":342.063,\"z\":195.686}', NULL, '[\"apa_v_mp_h_02_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-774.4,\"y\":342.0,\"z\":195.7}', '{\"x\":-765.303,\"y\":330.932,\"z\":195.085}', '{\"x\":-763.1,\"y\":328.8,\"z\":198.5}', 1300000),
+(19, 'Vibrant1Apartment', 'Appartement Vibrant 1', NULL, '{\"x\":-784.194,\"y\":323.636,\"z\":210.997}', '{\"x\":-779.751,\"y\":323.385,\"z\":210.997}', NULL, '[\"apa_v_mp_h_03_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-785.1,\"y\":323.6,\"z\":211.0}', '{\"x\":-765.885,\"y\":327.641,\"z\":210.396}', '{\"x\":-793.4,\"y\":326.6,\"z\":209.8}', 1300000),
+(20, 'Vibrant2Apartment', 'Appartement Vibrant 2', NULL, '{\"x\":-786.8663,\"y\":315.764,\"z\":186.913}', '{\"x\":-781.808,\"y\":315.866,\"z\":186.913}', NULL, '[\"apa_v_mp_h_03_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-786.9,\"y\":315.8,\"z\":186.9}', '{\"x\":-795.607,\"y\":327.344,\"z\":186.313}', '{\"x\":-798.0,\"y\":327.9,\"z\":189.7}', 1300000),
+(21, 'Vibrant3Apartment', 'Appartement Vibrant 3', NULL, '{\"x\":-774.012,\"y\":342.042,\"z\":195.686}', '{\"x\":-779.057,\"y\":342.063,\"z\":195.686}', NULL, '[\"apa_v_mp_h_03_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-774.4,\"y\":342.0,\"z\":195.7}', '{\"x\":-765.525,\"y\":330.851,\"z\":195.085}', '{\"x\":-762.9,\"y\":328.9,\"z\":198.5}', 1300000),
+(22, 'Sharp1Apartment', 'Appartement Persan 1', NULL, '{\"x\":-784.194,\"y\":323.636,\"z\":210.997}', '{\"x\":-779.751,\"y\":323.385,\"z\":210.997}', NULL, '[\"apa_v_mp_h_04_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-784.8,\"y\":323.8,\"z\":211.0}', '{\"x\":-766.527,\"y\":327.89,\"z\":210.396}', '{\"x\":-793.4,\"y\":327.0,\"z\":209.8}', 1300000),
+(23, 'Sharp2Apartment', 'Appartement Persan 2', NULL, '{\"x\":-786.8663,\"y\":315.764,\"z\":186.913}', '{\"x\":-781.808,\"y\":315.866,\"z\":186.913}', NULL, '[\"apa_v_mp_h_04_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-786.5,\"y\":315.8,\"z\":186.9}', '{\"x\":-795.642,\"y\":326.497,\"z\":186.313}', '{\"x\":-797.8,\"y\":328.3,\"z\":189.7}', 1300000),
+(24, 'Sharp3Apartment', 'Appartement Persan 3', NULL, '{\"x\":-774.012,\"y\":342.042,\"z\":195.686}', '{\"x\":-779.057,\"y\":342.063,\"z\":195.686}', NULL, '[\"apa_v_mp_h_04_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-774.6,\"y\":342.0,\"z\":195.7}', '{\"x\":-765.503,\"y\":331.318,\"z\":195.085}', '{\"x\":-763.3,\"y\":329.0,\"z\":198.5}', 1300000),
+(25, 'Monochrome1Apartment', 'Appartement Monochrome 1', NULL, '{\"x\":-784.194,\"y\":323.636,\"z\":210.997}', '{\"x\":-779.751,\"y\":323.385,\"z\":210.997}', NULL, '[\"apa_v_mp_h_05_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-784.9,\"y\":323.7,\"z\":211.0}', '{\"x\":-766.289,\"y\":328.086,\"z\":210.396}', '{\"x\":-793.3,\"y\":327.4,\"z\":209.8}', 1300000),
+(26, 'Monochrome2Apartment', 'Appartement Monochrome 2', NULL, '{\"x\":-786.8663,\"y\":315.764,\"z\":186.913}', '{\"x\":-781.808,\"y\":315.866,\"z\":186.913}', NULL, '[\"apa_v_mp_h_05_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-787.1,\"y\":315.7,\"z\":186.9}', '{\"x\":-795.692,\"y\":326.762,\"z\":186.313}', '{\"x\":-797.7,\"y\":328.8,\"z\":189.7}', 1300000),
+(27, 'Monochrome3Apartment', 'Appartement Monochrome 3', NULL, '{\"x\":-774.012,\"y\":342.042,\"z\":195.686}', '{\"x\":-779.057,\"y\":342.063,\"z\":195.686}', NULL, '[\"apa_v_mp_h_05_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-774.6,\"y\":342.0,\"z\":195.7}', '{\"x\":-765.094,\"y\":330.976,\"z\":195.085}', '{\"x\":-763.1,\"y\":329.1,\"z\":198.5}', 1300000),
+(28, 'Seductive1Apartment', 'Appartement Séduisant 1', NULL, '{\"x\":-784.194,\"y\":323.636,\"z\":210.997}', '{\"x\":-779.751,\"y\":323.385,\"z\":210.997}', NULL, '[\"apa_v_mp_h_06_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-785.1,\"y\":323.7,\"z\":211.0}', '{\"x\":-766.263,\"y\":328.104,\"z\":210.396}', '{\"x\":-793.5,\"y\":326.8,\"z\":209.8}', 1300000),
+(29, 'Seductive2Apartment', 'Appartement Séduisant 2', NULL, '{\"x\":-786.8663,\"y\":315.764,\"z\":186.913}', '{\"x\":-781.808,\"y\":315.866,\"z\":186.913}', NULL, '[\"apa_v_mp_h_06_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-786.0,\"y\":315.9,\"z\":186.9}', '{\"x\":-795.655,\"y\":326.611,\"z\":186.313}', '{\"x\":-797.8,\"y\":328.9,\"z\":189.7}', 1300000),
+(30, 'Seductive3Apartment', 'Appartement Séduisant 3', NULL, '{\"x\":-774.012,\"y\":342.042,\"z\":195.686}', '{\"x\":-779.057,\"y\":342.063,\"z\":195.686}', NULL, '[\"apa_v_mp_h_06_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-774.6,\"y\":342.0,\"z\":195.7}', '{\"x\":-765.3,\"y\":331.414,\"z\":195.085}', '{\"x\":-763.0,\"y\":329.2,\"z\":198.5}', 1300000),
+(31, 'Regal1Apartment', 'Appartement Régal 1', NULL, '{\"x\":-784.194,\"y\":323.636,\"z\":210.997}', '{\"x\":-779.751,\"y\":323.385,\"z\":210.997}', NULL, '[\"apa_v_mp_h_07_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-784.9,\"y\":323.6,\"z\":211.0}', '{\"x\":-765.956,\"y\":328.257,\"z\":210.396}', '{\"x\":-793.5,\"y\":327.1,\"z\":209.8}', 1300000),
+(32, 'Regal2Apartment', 'Appartement Régal 2', NULL, '{\"x\":-786.8663,\"y\":315.764,\"z\":186.913}', '{\"x\":-781.808,\"y\":315.866,\"z\":186.913}', NULL, '[\"apa_v_mp_h_07_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-786.2,\"y\":315.6,\"z\":186.9}', '{\"x\":-795.545,\"y\":326.659,\"z\":186.313}', '{\"x\":-797.8,\"y\":327.7,\"z\":189.7}', 1300000),
+(33, 'Regal3Apartment', 'Appartement Régal 3', NULL, '{\"x\":-774.012,\"y\":342.042,\"z\":195.686}', '{\"x\":-779.057,\"y\":342.063,\"z\":195.686}', NULL, '[\"apa_v_mp_h_07_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-774.9,\"y\":342.0,\"z\":195.7}', '{\"x\":-765.087,\"y\":331.429,\"z\":195.123}', '{\"x\":-763.4,\"y\":329.3,\"z\":198.5}', 1300000),
+(34, 'Aqua1Apartment', 'Appartement Aqua 1', NULL, '{\"x\":-784.194,\"y\":323.636,\"z\":210.997}', '{\"x\":-779.751,\"y\":323.385,\"z\":210.997}', NULL, '[\"apa_v_mp_h_08_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-785.0,\"y\":323.7,\"z\":211.0}', '{\"x\":-766.187,\"y\":328.47,\"z\":210.396}', '{\"x\":-793.4,\"y\":327.6,\"z\":209.8}', 1300000),
+(35, 'Aqua2Apartment', 'Appartement Aqua 2', NULL, '{\"x\":-786.8663,\"y\":315.764,\"z\":186.913}', '{\"x\":-781.808,\"y\":315.866,\"z\":186.913}', NULL, '[\"apa_v_mp_h_08_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-786.3,\"y\":315.8,\"z\":186.9}', '{\"x\":-795.658,\"y\":326.563,\"z\":186.313}', '{\"x\":-797.8,\"y\":328.6,\"z\":189.7}', 1300000),
+(36, 'Aqua3Apartment', 'Appartement Aqua 3', NULL, '{\"x\":-774.012,\"y\":342.042,\"z\":195.686}', '{\"x\":-779.057,\"y\":342.063,\"z\":195.686}', NULL, '[\"apa_v_mp_h_08_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-774.4,\"y\":341.9,\"z\":195.7}', '{\"x\":-765.287,\"y\":331.084,\"z\":195.086}', '{\"x\":-763.2,\"y\":329.6,\"z\":198.5}', 1300000),
+(37, 'IntegrityWay', '4 Integrity Way', '{\"x\":-47.804,\"y\":-585.867,\"z\":36.956}', NULL, NULL, '{\"x\":-54.178,\"y\":-583.762,\"z\":35.798}', '[]', NULL, 0, 0, 1, NULL, NULL, NULL, 0),
+(38, 'IntegrityWay28', '4 Integrity Way - Apt 28', NULL, '{\"x\":-31.409,\"y\":-594.927,\"z\":79.03}', '{\"x\":-26.098,\"y\":-596.909,\"z\":79.03}', NULL, '[]', 'IntegrityWay', 0, 1, 0, '{\"x\":-31.1,\"y\":-595.1,\"z\":79.0}\r\n', '{\"x\":-11.923,\"y\":-597.083,\"z\":78.43}', '{\"x\":-38.4,\"y\":-589.7,\"z\":77.8}', 1700000),
+(39, 'IntegrityWay30', '4 Integrity Way - Apt 30', NULL, '{\"x\":-17.702,\"y\":-588.524,\"z\":89.114}', '{\"x\":-16.21,\"y\":-582.569,\"z\":89.114}', NULL, '[]', 'IntegrityWay', 0, 1, 0, '{\"x\":-17.3,\"y\":-588.1,\"z\":89.1}', '{\"x\":-26.327,\"y\":-588.384,\"z\":89.123}', '{\"x\":-38.0,\"y\":-583.6,\"z\":82.9}', 1700000),
+(40, 'DellPerroHeights', 'Dell Perro Heights', '{\"x\":-1447.06,\"y\":-538.28,\"z\":33.74}', NULL, NULL, '{\"x\":-1440.022,\"y\":-548.696,\"z\":33.74}', '[]', NULL, 0, 0, 1, NULL, NULL, NULL, 0),
+(41, 'DellPerroHeightst4', 'Dell Perro Heights - Apt 28', NULL, '{\"x\":-1452.125,\"y\":-540.591,\"z\":73.044}', '{\"x\":-1455.435,\"y\":-535.79,\"z\":73.044}', NULL, '[]', 'DellPerroHeights', 0, 1, 0, '{\"x\":-1452.8,\"y\":-540.2,\"z\":73.0}', '{\"x\":-1467.058,\"y\":-527.571,\"z\":72.443}', '{\"x\":-1450.6,\"y\":-549.5,\"z\":71.8}', 1700000),
+(42, 'DellPerroHeightst7', 'Dell Perro Heights - Apt 30', NULL, '{\"x\":-1451.562,\"y\":-523.535,\"z\":55.928}', '{\"x\":-1456.02,\"y\":-519.209,\"z\":55.929}', NULL, '[]', 'DellPerroHeights', 0, 1, 0, '{\"x\":-1452.2,\"y\":-523.2,\"z\":55.9}', '{\"x\":-1457.026,\"y\":-530.219,\"z\":55.937}', '{\"x\":-1467.7,\"y\":-538.0,\"z\":49.7}', 1700000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `properties`
+--
+ALTER TABLE `properties`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `properties`
+--
+ALTER TABLE `properties`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
